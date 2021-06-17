71d03935a0738954f4594f6c035f586978e5ae9a
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2021-06-17 16:03:31 +0000

    Update request for loop for user until user enters valid data

--- run.py
+++ run.py
@@ -14,15 +14,24 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
-    print("Please enter the sales data from the last market.")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example:10,20,30,40,50,60\n")
+    while True:
+        print("Please enter the sales data from the last market.")
+        print("Data should be six numbers, seperated by commas.")
+        print("Example:10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+    return sales_data
 
 def validate_data(values):
     """
@@ -30,7 +39,7 @@ def validate_data(values):
     Raises ValueError if strings cannot  be converted into int, or if there aren't exactly 6 values.
 
     """
-    print(values)
+    
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -38,6 +47,8 @@ def validate_data(values):
         )    
     except ValueError as e:
         print(f"Invalid Data:{e}, please try again.\n")
-   
+        return False
+    
+    return True
 
-get_sales_data()
+data = get_sales_data()
